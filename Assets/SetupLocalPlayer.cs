2d01442f0b1ee05103c82e91a4bfdb493a58b1f3
genus1 <roger@rogersbooks.info>
2017-12-15 10:25:55 -0800

    Network Players and Camera Follow
    Basic Networking with Player and camera follow.

---
+++ Assets/SetupLocalPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		if(isLocalPlayer)
+		{
+			GetComponent<PlayerController>().enabled = true;
+            CameraFollow360.player = this.gameObject.transform;
+		}
+		else
+		{
+			GetComponent<PlayerController>().enabled = false;
+		}
+	}
+
+}
