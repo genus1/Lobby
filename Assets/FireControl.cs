c007c8ae751ffcc3d5c181bb1cd2f7ada34774df
genus1 <roger@rogersbooks.info>
2017-12-16 00:41:09 -0800

    Add Gun and Bullets
    Add a Gun Spon asset and bullets along with network firing.

---
+++ Assets/FireControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class FireControl : NetworkBehaviour {
+
+    public GameObject bulletPrefab;
+    public GameObject bulletSpawn;
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer) return;
+
+        if (Input.GetKeyDown("space"))
+        {
+            CmdShoot();
+        }
+    }
+
+    [Command]
+    private void CmdShoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        Quaternion q = Quaternion.FromToRotation(Vector3.up, transform.forward);
+        bullet.transform.rotation = q * bullet.transform.rotation;
+        //bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.transform.forward * 4000);
+        bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.transform.forward * 75;
+        NetworkServer.Spawn(bullet);
+
+        Destroy(bullet, 10.0f);
+    }
+
+}
