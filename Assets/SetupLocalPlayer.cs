999cb95c9532b8250f80ab3e7f7a6142a36268f6
genus1 <roger@rogersbooks.info>
2017-12-15 16:55:40 -0800

    Add Player Name
    Add Name to player facing camera.

--- Assets/SetupLocalPlayer.cs
+++ Assets/SetupLocalPlayer.cs
@@ -1,11 +1,42 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class SetupLocalPlayer : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () 
+    public Text namePrefab;
+    public Text nameLabel;
+    public Transform namePos;
+    string textboxname = "";
+
+    [SyncVar (hook = "OnChangeName")]
+    public string pName = "player";
+
+    void OnChangeName(string n) {
+        pName = n;
+        nameLabel.text = pName;
+    }
+
+    [Command]
+    public void CmdChangeName(string newName)
+    {
+        pName = newName;
+        nameLabel.text = pName;
+    }
+
+
+    private void OnGUI()
+    { 
+        if (isLocalPlayer)
+        {
+            textboxname = GUI.TextField(new Rect(25, 15, 100, 25), textboxname);
+            if (GUI.Button(new Rect(130, 15, 35, 25), "Set"))
+                CmdChangeName(textboxname);
+        }
+    }
+
+    // Use this for initialization
+    void Start () 
 	{
 		if(isLocalPlayer)
 		{
@@ -16,6 +47,17 @@ public class SetupLocalPlayer : NetworkBehaviour {
 		{
 			GetComponent<PlayerController>().enabled = false;
 		}
+
+        GameObject canvas = GameObject.FindWithTag("MainCanvas");
+        nameLabel = Instantiate(namePrefab, Vector3.zero, Quaternion.identity) as Text;
+        nameLabel.transform.SetParent(canvas.transform);
+
 	}
+
+    private void Update()
+    {
+        Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(namePos.position);
+        nameLabel.transform.position = nameLabelPos;
+    }
 
 }
