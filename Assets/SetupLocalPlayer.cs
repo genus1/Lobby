684557f6a0112fcc4bd325fc5fcb3a2a048f577e
genus1 <roger@rogersbooks.info>
2017-12-15 19:56:08 -0800

    Adjust Name and Color
    Resize name, set color of player, improved startup and jitter follow.

--- Assets/SetupLocalPlayer.cs
+++ Assets/SetupLocalPlayer.cs
@@ -8,22 +8,75 @@ public class SetupLocalPlayer : NetworkBehaviour {
     public Text nameLabel;
     public Transform namePos;
     string textboxname = "";
+    string colourboxname = "";
 
-    [SyncVar (hook = "OnChangeName")]
-    public string pName = "player";
-
-    void OnChangeName(string n) {
-        pName = n;
-        nameLabel.text = pName;
+    //Initialize sync on start
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        Invoke("UpdateStates", 1);
     }
 
-    [Command]
-    public void CmdChangeName(string newName)
+    void UpdateStates()
     {
+        OnChangeName(pName);
+        OnChangeColour(pColour);
+    }
+
+    //Change Name Code
+
+    [Command]
+    public void CmdChangeName(string newName){
         pName = newName;
         nameLabel.text = pName;
     }
 
+    [SyncVar (hook = "OnChangeName")]
+    public string pName = "player";
+
+    void OnChangeName(string name) {
+        pName = name;
+        nameLabel.text = pName;
+    }
+
+    //Change Color Code
+
+    [Command]
+    public void CmdChangeColour(string newColour){
+        SetColor(newColour);
+    }
+
+    [SyncVar(hook = "OnChangeColour")]
+    public string pColour = "#ffffff";
+
+    void OnChangeColour(string newColour){
+        SetColor(newColour);
+    }
+
+    private void SetColor(string newColour){
+        pColour = newColour;
+        Renderer[] rends = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer r in rends){
+            if (r.gameObject.name == "BODY")
+                r.material.SetColor("_Color", ColorFromHex(pColour));
+        }
+    }
+
+    //Credit for method:  http://answers.unity3d.com/questions/812240
+    //hex for testing -- green:04BF3404 red: 9F121204 blue: 221E9004
+    Color ColorFromHex(string hex){
+        hex = hex.Replace("0x", "");
+        hex = hex.Replace("#", "");
+        byte a = 255;
+        byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+        byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+        byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+        if (hex.Length == 8){
+            a = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+        }
+        return new Color32(r, g, b, a);
+    }
 
     private void OnGUI()
     { 
@@ -32,6 +85,10 @@ public class SetupLocalPlayer : NetworkBehaviour {
             textboxname = GUI.TextField(new Rect(25, 15, 100, 25), textboxname);
             if (GUI.Button(new Rect(130, 15, 35, 25), "Set"))
                 CmdChangeName(textboxname);
+
+            colourboxname = GUI.TextField(new Rect(170, 15, 100, 25), colourboxname);
+            if (GUI.Button(new Rect(275, 15, 35, 25), "Set"))
+                CmdChangeColour(colourboxname);
         }
     }
 
@@ -56,8 +113,24 @@ public class SetupLocalPlayer : NetworkBehaviour {
 
     private void Update()
     {
-        Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(namePos.position);
-        nameLabel.transform.position = nameLabelPos;
+        //determine if the object is inside the camera's viewing volume
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(this.transform.position);
+        bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 &&
+            screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+        if (onScreen)
+        {
+            Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(namePos.position);
+            nameLabel.transform.position = nameLabelPos;
+        } else
+        {
+            nameLabel.transform.position = new Vector3(-1000, -1000, 0);
+        }     
+    }
+
+    public void OnDestroy()
+    {
+        if (nameLabel != null)
+            Destroy(nameLabel.gameObject);
     }
 
 }
